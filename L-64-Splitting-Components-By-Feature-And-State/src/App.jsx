c495e08c564668e47c6a03123b0d830148bfd703
Singh <anshus@netapp.com>
2024-07-02 23:03:16 +0530

    L-64

--- L-64-Splitting-Components-By-Feature-And-State/src/App.jsx
+++ L-64-Splitting-Components-By-Feature-And-State/src/App.jsx
@@ -1,8 +1,5 @@
 import Header from './components/Header/Header.jsx'; // Importing Header component
-import TabButton from './components/TabButton.jsx';
 import CoreConcepts from './components/CoreConcepts.jsx';
-import { useState, Fragment } from 'react'; // Importing useState hook
-import { EXAMPLES } from './data.js'; // Importing EXAMPLES data
 import Examples from './components/Examples.jsx';
 
 function App() {
